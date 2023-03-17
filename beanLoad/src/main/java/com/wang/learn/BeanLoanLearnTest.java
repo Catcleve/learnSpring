504d48b9dfc4e91857a8a76eef29c30c820f5bb1
wangys <wangys@baihang-china.com>
2023-03-17 22:51:49 +0800

    2023年3月17日：学习第五章bean的加载 跳过了第四章自定义属性加载 学习至5.3 学习了getBean的前几步 从缓存中加载

--- beanLoad/src/main/java/com/wang/learn/BeanLoanLearnTest.java
+++ beanLoad/src/main/java/com/wang/learn/BeanLoanLearnTest.java
@@ -2,6 +2,7 @@ package com.wang.learn;
 
 import com.wang.learn.bean.TestBean;
 import org.junit.Test;
+import org.springframework.beans.factory.support.AbstractBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -14,7 +15,15 @@ import org.springframework.core.io.ClassPathResource;
 public class BeanLoanLearnTest {
 
 
-        @Test
+    /**
+     * {@link AbstractBeanFactory#transformedBeanName}解析名称
+     *
+     * 学习了getBean的前几步 从缓存中加载，如何没有就存缓存的factoryBean中加载，然后执行 getObject方法来获取bean
+     * 在返回对应的bean之前，会在finally中执行 BeanPostProcessor的postProcessAfterInitialization方法，也就是bean的后置处理器
+     * 可以通过实现 BeanPostProcessor来执行自己业务逻辑
+     * 这些全是从缓存中加载到bean的处理，如果缓存中加载不到，则执行从头加载的过程，见5.4学习
+     */
+    @Test
         public void testAlias() {
 
             ClassPathResource resource = new ClassPathResource("beanFactoryTest.xml");
--- factorybean/src/test/java/com/example/factorybean/FactorybeanApplicationTests.java
+++ factorybean/src/test/java/com/example/factorybean/FactorybeanApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.factorybean;
 
+import com.example.factorybean.bean.TestBean;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.AbstractBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
@@ -11,10 +14,22 @@ class FactorybeanApplicationTests {
     @Autowired
     ApplicationContext applicationContext;
 
+    /**
+     * 第一步解析名称
+     * {@link AbstractBeanFactory#transformedBeanName}
+     * getBean参数如果前面加上 "&"符号，则会返回bean对应的 factoryBean
+     * @throws Exception 异常
+     */
     @Test
-    void contextLoads() {
+    void getBeanTest() throws Exception {
         Object fac = applicationContext.getBean("fac");
         System.out.println("fac = " + fac);
+
+        FactoryBean<?> bean = (FactoryBean<?>) applicationContext.getBean("&fac");
+        TestBean object = (TestBean)bean.getObject();
+        System.out.println(bean.getObjectType());
+        System.out.println(fac == object);
+        System.out.println(System.identityHashCode(fac));
     }
 
 }
