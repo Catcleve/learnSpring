8823d1a57598ae87ec62590569185274353a0865
wangys <wangys@baihang-china.com>
2023-03-19 11:18:51 +0800

    2023年3月18日：学习第五章bean的加载 跳过了第四章自定义属性加载 学习至5.6 了解了循环依赖出现的原因，处理方法，以及各种情况下的处理，详见 package com.example.circleref.package-info.java;

---
+++ circleref/src/main/java/com/example/circleref/CirclerefApplication.java
@@ -0,0 +1,13 @@
+package com.example.circleref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirclerefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CirclerefApplication.class, args);
+    }
+
+}
---
+++ circleref/src/test/java/com/example/circleref/CirclerefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest()
+class CirclerefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
