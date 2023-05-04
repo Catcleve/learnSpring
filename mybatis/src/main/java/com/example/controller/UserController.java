93d6a29c354cf20201378b1c50676d6adc36b6ae
wangys <wangys@baihang-china.com>
2023-05-04 21:38:42 +0800

    2023年5月4日：第十章跳过 讲的事务 过于复杂，学习springmvc相关知识，了解为主

--- mybatis/src/main/java/com/example/controller/UserController.java
+++ mybatis/src/main/java/com/example/controller/UserController.java
@@ -5,11 +5,14 @@ import com.example.dao.UserDao;
 import com.example.dto.User;
 import com.example.dynamic.context.DynamicDataSourceContextHolder;
 import com.example.service.IUserService;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.List;
 
 @RestController
@@ -20,8 +23,13 @@ public class UserController {
     @Resource
     private IUserService userService;
 
+    @Resource
+    private HttpServletRequest  request;
+
     @RequestMapping("/listAll")
-    public CommonResult<?> getUser(@RequestParam String source){
+    public CommonResult<?> getUser(@RequestParam String source, HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        request.getCookies();
         DynamicDataSourceContextHolder.setDataSourceType(source);
         List<User> all;
         try {
