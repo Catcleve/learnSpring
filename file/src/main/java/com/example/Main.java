8e55db3f1fa98e5256ac72bdb7c8f6737405eedd
wangys <wangys@baihang-china.com>
2023-07-14 16:51:23 +0800

    2023年7月14日：文件处理测试学习

--- file/src/main/java/com/example/Main.java
+++ file/src/main/java/com/example/Main.java
@@ -1,10 +1,20 @@
 package com.example;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//enterPrise.base.info.url=http://open.api.tianyancha.com/services/v3/open/baseinfo
+        sss();
+    }
+
+    private static void extracted() {
         String s = "        location ^~ %s {\n" +
                 "                proxy_pass %s;\n" +
                 "        }";
@@ -13,7 +23,7 @@ public class Main {
         //读取一行并处理
         String line = null;
         try (BufferedReader reader = new BufferedReader(new FileReader("D:\\api.txt"));
-                     BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\result.txt"));){
+             BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\result.txt"));) {
             while ((line = reader.readLine()) != null) {
                 //处理这行
                 String s1 = line.split("=")[1];
@@ -27,6 +37,23 @@ public class Main {
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void sss() {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try (InputStream inputStream = Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("23062800004json.txt"));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+             OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("D:\\23062800004json.pdf"));) {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            JSONObject entries = JSONUtil.parseObj(sb);
+            byte[] files = entries.getBytes("invoiceFile");
+            outputStream.write(files);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
